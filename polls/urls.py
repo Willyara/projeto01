fb4a6737d0ebc15bb2f62f6bf17071d92d98fac9
Willyara <78544557+Willyara@users.noreply.github.com>
2021-11-22 11:38:48 -0300

    aula2

--- polls/urls.py
+++ polls/urls.py
@@ -5,6 +5,9 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('detalhe',views.detalhe, name='detalhe'),
+    path('resultados',views.resultados, name='resultados'),
+    path('voto',views.voto, name='voto'),
 ]
 
 
--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Olá mundo, essa é uma resposta HTTP')
+    return HttpResponse('Olá mundo! Essa é uma resposta HTTP.')
+
+def detalhe(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def resultados(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def voto(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
