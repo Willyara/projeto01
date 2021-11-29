298e1e5556c1afae9a8106aa47f57d48fd36abd3
Willyara <78544557+Willyara@users.noreply.github.com>
2021-11-29 11:54:35 -0300

    quase tudo ok

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
@@ -25,6 +25,19 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "Você não selecionou uma escolha.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))  
 
 #def index(request):
  #   latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -61,19 +74,6 @@ class ResultsView(generic.DetailView):
     #}
     #return HttpResponse(template.render(context, request))
 
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "Você não selecionou uma escolha.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))  
 #def results(request, question_id):
   #  question = get_object_or_404(Question, pk=question_id)
     #return render(request, 'polls/results.html', {'question': question})
